5b1e99bd0ad72fca2551cbae10a75803ddc2052c
Rishon73 <sshiff@gmail.com>
2018-07-11 14:12:34 -0400

    added tags and isUsingTunnel. Changing the call for initParamsFromJSONConfigFile

--- src/main/java/com/mf/Environment.java
+++ src/main/java/com/mf/Environment.java
@@ -2,6 +2,7 @@ package com.mf;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.JSONArray;
 import org.json.simple.parser.ParseException;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -14,30 +15,30 @@ public class Environment {
     private String browser = "firefox";
     private String version = "latest";
     private String platform = "Windows 10";
-    private String testName = "Selenium Burst JAVA";
+    private String testName = "My Test";
     private String resolution = "800x600";
+    private String tunnelName = "My Tunnel";
     private URL SeleniumURL;
-    private String tunnelName;
     private String srfClientID;
     private String srfClientSecret;
     private String buildNumber;
     private String release;
+    private boolean isUsingTunnel;
+    private String[] tags;
 
-    public Environment(String configFilePath) {
-        initParamsFromJSONConfigFile(configFilePath);
-    }
+    public Environment() { }
 
     public String AllEnvironmentToString(){
         return String.format("Test name: %s\nSRF_CLIENT_ID: %s\n" +
                         "SRF_CLIENT_SECRET: %s\n" + "SeleniumURL: %s\nBuild number: %s\nRelease: %s\n" +
-                        "Browser: %s\nVersion: %s\nPlatform: %s\nResolutions: %s",
-                getTestName(), getSrfClientId(), getSrfClientSecret(), getSeleniumURL(),
-                getBuildNumber(), getRelease(), getBrowser(), getVersion(), getPlatform(), getResolution());
+                        "Browser: %s\nVersion: %s\nPlatform: %s\nResolutions: %s\nTunnel name: %s\n" +
+                        "Using tunnel? %s\nTags: %s\n",
+                getTestName(), getSrfClientId(), getSrfClientSecret(), getSeleniumURL(), getBuildNumber(),
+                getRelease(), getBrowser(), getVersion(), getPlatform(), getResolution(), getTunnelName(),
+                String.valueOf(getIsUsingTunnel()), java.util.Arrays.toString(getTags()));
     }
 
-    public String getBrowser() {
-        return browser;
-    }
+    public String getBrowser() { return browser; }
 
     public void setBrowser(String browser) {
         this.browser = browser;
@@ -105,14 +106,28 @@ public class Environment {
 
     public String getRelease() { return release; }
 
+    public void setIsUsingTunnel(boolean isUsingTunnel) { this.isUsingTunnel = isUsingTunnel;}
+
+    public boolean getIsUsingTunnel() { return isUsingTunnel; }
+
+    public void setTags(JSONArray tagsArray) {
+        String[] tags = new String[tagsArray.size()];
+        for (int i = 0; i < tagsArray.size(); i++) {
+            tags[i] = (String)tagsArray.get(i);
+        }
+        this.tags = tags;
+    }
+
+    public String[] getTags() { return tags; }
 
     // init the local variables from a json file
-    private void initParamsFromJSONConfigFile(String filePath) {
+    public void initParamsFromJSONConfigFile(String filePath) {
         System.out.println("Parsing file: " + filePath);
         JSONParser parser = new JSONParser();
 
         try {
             JSONObject jsonObject = (JSONObject) parser.parse(new FileReader(filePath));
+
             setSeleniumURL(new URL(jsonObject.get("SeleniumURL").toString()));
             setTunnelName(jsonObject.get("tunnel_name").toString());
             setSrfClientId(jsonObject.get("CLIENT_ID").toString());
@@ -123,6 +138,9 @@ public class Environment {
             setBuildNumber(jsonObject.get("buildNumber").toString());
             setPlatform(jsonObject.get("platform").toString());
             setResolution(jsonObject.get("resolution").toString());
+            setIsUsingTunnel(jsonObject.get("usingTunnel").toString().equals("true"));
+            setTags((JSONArray)jsonObject.get("tags"));
+            setTestName(jsonObject.get("testName").toString());
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -136,6 +154,9 @@ public class Environment {
     // init the DesiredCapabilities object for the correct browser
     public DesiredCapabilities initBrowserCapabilities(String browser) {
         DesiredCapabilities capabilities;
+        System.out.println("Inside initBrowserCapabilities() - browser is " + browser);
+        if (browser == null) browser = this.getBrowser();
+
         if(browser.toLowerCase().equals("chrome"))
             capabilities = DesiredCapabilities.chrome();
         else if (browser.toLowerCase().equals("firefox"))
